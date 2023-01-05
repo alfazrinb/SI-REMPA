8d275baf400cf610f6c1fc40034cb3554b92f609
Alfazrin Banapon <36952585+alfazrinb@users.noreply.github.com>
2023-01-06 02:13:07 +0900

    Update SI-REMPA.py

--- SI-REMPA.py
+++ SI-REMPA.py
@@ -14,22 +14,22 @@ from streamlit_option_menu import option_menu
 mutasi_model = pickle.load(open("model_mutasi_pegawai_fix_banget.sav", "rb"))
 mutasi_Satker_model = pickle.load(open("model_mutasi_satker.sav", "rb"))
 
-# import base64
-# def add_bg_from_local(image_file):
-#     with open(image_file, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#     st.markdown(
-#     f"""
-#     <style>
-#     .stApp {{
-#         background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
-#         background-size: cover
-#     }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-#     )
-# add_bg_from_local('BC6.jpeg')   
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('BC6.jpeg')   
 
 # sidebar for navigation
 with st.sidebar:
